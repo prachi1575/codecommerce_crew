0e7b0a606e1d9d6420178a3e866018154f9ce698
prabhsimran11 <prabhkaur1902@gmail.com>
2025-04-28 14:19:05 +0530

    menu javascript

---
+++ menu.js
@@ -0,0 +1,12 @@
+
+const menuButton = document.getElementById('menu-button');
+const closeButton = document.getElementById('close-button');
+const sideMenu = document.getElementById('side-menu');
+
+menuButton.addEventListener('click', () => {
+  sideMenu.classList.add('open');
+});
+
+closeButton.addEventListener('click', () => {
+  sideMenu.classList.remove('open');
+});
