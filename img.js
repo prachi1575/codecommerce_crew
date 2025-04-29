8a5af73222ae95517fce39574d04441b4d9b4c5d
prabhsimran11 <prabhkaur1902@gmail.com>
2025-04-29 12:12:45 +0530

    image javascript with zoomable-image javascript

--- img.js
+++ img.js
@@ -1,9 +1,10 @@
-
-document.addEventListener('DOMContentLoaded', () => {
+// Ensure the DOM content is fully loaded before executing the script
+document.addEventListener('DOMContentLoaded', () => { 
+// Select the image element with the 'zoomable-image' class
     const image = document.querySelector('.zoomable-image');
 
+ // Add a click event listener to toggle the 'zoom-out' class on the image
     image.addEventListener('click', () => {
         image.classList.toggle('zoom-out');
     });
 });
-
